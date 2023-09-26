0b958c409a8560bd79153fc280c3a8d15635fab1
deliacirstea <deliutacirstea@gmail.com>
2023-09-26 17:30:11 +0200

    final push on branch

--- todo-app/src/services/todoServices.ts
+++ todo-app/src/services/todoServices.ts
@@ -4,7 +4,7 @@ const baseUrl = `${process.env.REACT_APP_API_URL}/api/Todo`;
 
 export const createTodo = async(todo : {title: string; completed:boolean;}) => {
     return await axios.post(baseUrl, {
-        title: todo.title,
+        text: todo.title,
         completed: todo.completed
     }).then(response => response.data);
 }
